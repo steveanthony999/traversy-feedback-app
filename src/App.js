fd62c3a4e189d48b6c6575b6c3a09c8896ffeb7b
Steven Woodward <steveanthony999@gmail.com>
2022-03-07 23:27:22 -0800

    set up header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React from 'react';
+import Header from './components/Header';
 
 const App = () => {
-  const comments = [
-    { id: 1, text: 'Comment one' },
-    { id: 2, text: 'Comment two' },
-    { id: 3, text: 'Comment three' },
-  ];
   return (
-    <div>
-      <h1>Hello from the App Component</h1>
-      <h1>Comments ({comments.length})</h1>
-      <ul>
-        {comments.map((x, index) => (
-          <li key={index}>{x.text}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Header />
+      <h1>My App</h1>
+    </>
   );
 };
 
