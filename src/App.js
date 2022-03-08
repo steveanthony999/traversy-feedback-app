2bdc8056c3fb04fd9b35bddcd4984abe817b8452
Steven Woodward <steveanthony999@gmail.com>
2022-03-07 22:06:34 -0800

    playing around with props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
+  const comments = [
+    { id: 1, text: 'Comment one' },
+    { id: 2, text: 'Comment two' },
+    { id: 3, text: 'Comment three' },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello from the App Component</h1>
+      <h1>Comments ({comments.length})</h1>
+      <ul>
+        {comments.map((x, index) => (
+          <li key={index}>{x.text}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
