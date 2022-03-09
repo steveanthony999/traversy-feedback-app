987d5d6bbba266f58aabbe4761c7b918fa8d07a4
Steven Woodward <steveanthony999@gmail.com>
2022-03-08 22:08:48 -0800

    set up card, card styles, and prop types

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 import FeedbackItem from './FeedbackItem';
 
 const FeedbackList = ({ feedback }) => {
@@ -12,6 +14,16 @@ const FeedbackList = ({ feedback }) => {
       ))}
     </div>
   );
+};
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
 };
 
 export default FeedbackList;
