f3737fb15195e26ef1ee5c2cc3ab037b89dc0afd
Steven Woodward <steveanthony999@gmail.com>
2022-03-08 22:30:28 -0800

    added delete functionality

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 import FeedbackItem from './FeedbackItem';
 
-const FeedbackList = ({ feedback }) => {
+const FeedbackList = ({ feedback, handleDelete }) => {
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet</p>;
   }
@@ -10,7 +10,7 @@ const FeedbackList = ({ feedback }) => {
   return (
     <div className='feedback-item'>
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   );
