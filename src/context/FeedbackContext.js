b28dcd076e3259283ef245b32c121b416d2be391
Steven Woodward <steveanthony999@gmail.com>
2022-03-10 14:39:56 -0800

    set up fetch to fetch feedback data from json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,21 +1,31 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is the default feedback',
-      rating: 10,
-    },
-  ]);
+  const [feedback, setFeedback] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  const fetchFeedback = async () => {
+    const res = await fetch(
+      `http://localhost:5000/feedback?_sort=id&_order=desc`
+    );
+
+    const data = await res.json();
+
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
@@ -46,6 +56,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback: feedback,
         feedbackEdit: feedbackEdit,
+        isLoading: isLoading,
         deleteFeedback: deleteFeedback,
         addFeedback: addFeedback,
         editFeedback: editFeedback,
